426e951973f023063e9028595bbeb578ea490fd0
Richard van der Dys <richard.vanderdys@gmail.com>
2019-05-10 18:01:16 +0300

    Blobs eat food

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext('2d');
 const maxSpeed = 5;
 const maxSize = 64;
 const maxSense = 64;
+const foodSize = 16;
 
 function createBlob() {
   return {
@@ -16,11 +17,33 @@ function createBlob() {
     turn: 0,
     size: 16,
     speed: 2,
-    sense: 32
+    sense: 32,
+    ate: 0
+  };
+}
+
+function createFood() {
+  return {
+    x: Math.random() * size,
+    y: Math.random() * size,
+    eaten: false
   };
 }
 
 const blobs = [createBlob(), createBlob(), createBlob()];
+
+const foods = [
+  createFood(),
+  createFood(),
+  createFood(),
+  createFood(),
+  createFood(),
+  createFood()
+];
+
+function contains(circle, pt) {
+  return Math.hypot(circle.x - pt.x, circle.y - pt.y) < circle.size;
+}
 
 function circle(x, y, r, c) {
   ctx.fillStyle = c;
@@ -38,9 +61,15 @@ function renderBlob({ x, y, speed, size, sense }) {
   circle(x, y, size, `rgb(${r}, ${g}, ${b})`);
 }
 
+function renderFood({ x, y, eaten }) {
+  if (eaten) return;
+  circle(x, y, foodSize, '#3eccf7');
+}
+
 function render() {
   ctx.fillStyle = '#ddd';
   ctx.fillRect(0, 0, size, size);
+  foods.forEach(renderFood);
   blobs.forEach(renderBlob);
 }
 
@@ -56,6 +85,11 @@ function updateBlob(blob) {
     blob.vx = randomDirection() * blob.speed;
     blob.vy = randomDirection() * blob.speed;
     blob.turn = Date.now();
+  }
+  const foodFound = foods.find(food => contains(blob, food));
+  if (foodFound && !foodFound.eaten) {
+    foodFound.eaten = true;
+    blob.ate++;
   }
 }
 
